0242872cb327413fbde77ae8bb2fd88c0cc596ba
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-13 01:42:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  return array = [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  return array.unshift(element)
+}
